870aa2294713af42236f151bdb58037fa234616e
manish thilagar <48599986+manishthilagar@users.noreply.github.com>
2020-08-24 09:38:54 +0530

    Update DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import io
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
